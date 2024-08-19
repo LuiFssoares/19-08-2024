63f4a293ef0f22cf6ce23cf3bef385a44cc06ea5
LuiFssoares <159054931+LuiFssoares@users.noreply.github.com>
2024-08-19 14:01:31 -0300

    Add files via upload

---
+++ SQLite.2.sql
@@ -0,0 +1,2 @@
+INSERT INTO produtos (nome, descrição, preco)
+VALUES ('notebook','notebook gamer com 16gb ram',4500.94);
---
+++ SQLite.3.sql
@@ -0,0 +1,2 @@
+INSERT INTO produtos (nome, descrição, preco)
+VALUES ('celular', 'celular gamer', 5000.50);
